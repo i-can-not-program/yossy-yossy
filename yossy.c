68838016834a9efb68eae623a2c639c92d635d64
Yoshi <austomus.minecraft@gmail.com>
2020-08-13 20:53:25 +1200

    Ask if you like yossy

--- yossy.c
+++ yossy.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAXLINE 1000
 int main(){
-    printf("A yossy yossy!\n");
+    char* name = (char *)malloc(MAXLINE * sizeof(char));
+    size_t size = MAXLINE;
+    printf("Give me some characters (your name)\n");
+    getline(&name,&size,stdin);
+    printf("Do you like yossy %s\n",name);
     return 0;
 }
